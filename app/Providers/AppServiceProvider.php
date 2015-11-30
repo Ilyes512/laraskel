2e1e6dfc98c7f49b76491acbce15782827822b72
Ilyes Ahidar <ilyes.ahidar@gmail.com>
2015-11-30 19:22:50 +0100

    Added barryvdh/laravel-ide-helper package for dev

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,13 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
+     * Will contain all development environment names
+     *
+     * @var array
+     */
+    protected $devEnvironments = [];
+
+    /**
      * Bootstrap any application services.
      *
      * @return void
@@ -21,6 +28,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->devEnvironments = $this->app->config->get('laraskel.devEnvironmentNames');
+
+        // Load these packages only in a dev environment
+        if ($this->app->environment($this->devEnvironments)) {
+            // Github Source: https://github.com/barryvdh/laravel-ide-helper
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+    }
     }
 }
---
+++ config/laraskel.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'devEnvironmentNames' => ['local', 'homestead', 'test', 'testing'],
+];
