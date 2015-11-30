eb2f852aaf3b55f7dd1fd925df2c3b3774a96114
Ilyes Ahidar <ilyes.ahidar@gmail.com>
2015-11-30 20:05:47 +0100

    Added filp/whoops dev package

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,9 +7,25 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Psr\Log\LoggerInterface;
+use Illuminate\Config\Repository as Config;
 
 class Handler extends ExceptionHandler
 {
+    /**
+     * The Config Repository
+     *
+     * @var \Illuminate\Config\Repository
+     */
+    protected $config;
+
+    public function __construct(LoggerInterface $log, Config $config)
+    {
+        $this->config = $config;
+
+        parent::__construct($log);
+    }
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -46,6 +62,34 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        if ($e instanceof ModelNotFoundException) {
+            $e = new NotFoundHttpException($e->getMessage(), $e);
+        }
+
+        // Check whether to render exception with Whoops
+        if ($this->config->get('app.debug') && $this->config->get('laraskel.debug.whoopsErrorReporting')) {
+            return $this->renderExceptionWithWhoops($e);
+        }
+
         return parent::render($request, $e);
+    }
+
+    /**
+     * Render an exception using Whoops.
+     *
+     * @param Exception $e
+     *
+     * @return \Illuminate\Http\Response
+     */
+    protected function renderExceptionWithWhoops(Exception $e)
+    {
+        $whoops = new \Whoops\Run;
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+
+        return new \Illuminate\Http\Response(
+            $whoops->handleException($e),
+            $e->getStatusCode(),
+            $e->getHeaders()
+        );
     }
 }
--- config/laraskel.php
+++ config/laraskel.php
@@ -3,6 +3,7 @@
 return [
     'devEnvironmentNames' => ['local', 'homestead', 'test', 'testing'],
     'debug'               => [
-        'debugbar' => ENV('DEBUG_DEBUGBAR', true),
+        'debugbar'             => ENV('DEBUG_DEBUGBAR', true),
+        'whoopsErrorReporting' => ENV('DEBUG_WHOOPS_ERROR_REPORTING', true),
     ]
 ];
